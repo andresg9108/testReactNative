6cf02a1685dde039adc8332111b0123cf6f587cb
Andrés González <andresg9108@outlook.com>
2024-04-12 12:10:33 -0500

    Revert "New version."
    This reverts commit cc9d4007b6c916a47edafc8731f6c8d0b009837e.

--- src/screens/FormsFormik/styles.js
+++ src/screens/FormsFormik/styles.js
@@ -2,19 +2,11 @@ import { StyleSheet } from 'react-native'
 import { font, fontSize, colorPalette } from '../../mainStyles.js'
 
 const styles = StyleSheet.create({
-  form: {
-    margin: 12
-  },
-  textInput: {
-    borderRadius: 5,
-    borderWidth: 2,
-    borderColor: '#fff',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginBottom: 20,
-    color: colorPalette.textColor1,
+  text: {
     fontFamily: font,
-    fontSize: fontSize
+    fontSize: fontSize * 2.2,
+    color: colorPalette.textColor1,
+    textAlign: 'center'
   }
 })
 
